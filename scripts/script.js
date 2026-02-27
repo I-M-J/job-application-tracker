22dd597bed673d3513c58dcefa9474bd30c0f749
I-M-J <149041998+I-M-J@users.noreply.github.com>
2026-02-28 05:55:09 +0600

    HTML Structure and Styling done, script writing in progress

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+jobList = document.getElementsByClassName('job');
+
+jobCount = jobList.length;
+
+totalCountEl = document.getElementById('total-count');
+
+totalCountEl.textContent = jobCount;
+
+console.log(jobCount);
+
